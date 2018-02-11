25e4cd2c0c6199086391a7964d1b6bd498464782
teng <zhang.ten@husky.neu.edu>
2018-02-11 17:03:46 -0500

    update

--- hw2/Implement Updates/Implement Updates 2.sql
+++ hw2/Implement Updates/Implement Updates 2.sql
@@ -3,6 +3,14 @@ SET
 `order` = CASE
 			WHEN widget.name = "head345" THEN 3
             WHEN NOT widget.name = "head345" 
-                 and widget.order > 1 THEN widget.order - 1
+                 and widget.order > 1 
+                 and widget.order <= 3 
+			THEN widget.order - 1
+            ELSE widget.order
 		  END
-WHERE widget.page = 345;
+WHERE widget.page = 
+(SELECT a.page
+FROM
+(SELECT widget.page 
+ FROM widget 
+ WHERE widget.name = "head345") AS a);
